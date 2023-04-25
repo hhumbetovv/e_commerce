3535b57d94953b6e4606e0ef90b3281a8da0cb74
hhumbetovv <hhumbetovv@gmail.com>
2023-04-25 23:43:22 +0400

    Detail navigation added to Favorite Product Card

--- lib/screens/favorites/components/favorite_product_card.dart
+++ lib/screens/favorites/components/favorite_product_card.dart
@@ -7,6 +7,7 @@ import '../../../constants/color_constants.dart';
 import '../../../enums/icons.dart';
 import '../../../models/product.dart';
 import '../../../widgets/app_inkwell.dart';
+import '../../product_details/product_details_view.dart';
 
 class FavoriteProductCard extends StatelessWidget {
   const FavoriteProductCard({
@@ -18,6 +19,16 @@ class FavoriteProductCard extends StatelessWidget {
   final ProductModel product;
   final Function(String) unFavorite;
 
+  void cardOnTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return ProductDetailsView(product: product);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -25,43 +36,54 @@ class FavoriteProductCard extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: Hero(
-                tag: product.imageUrls[0],
-                child: CachedNetworkImage(
-                  imageUrl: product.imageUrls[0],
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 16),
           Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.title,
-                  style: AppFonts.headingSmall,
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  '${product.price.toString()}\u20BC',
-                  style: AppFonts.bodyLarge,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  product.description,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
-                  style: AppFonts.bodyMedium.copyWith(color: ColorConstants.grey),
-                ),
-              ],
+            child: AppInkWell(
+              onTap: () => cardOnTap(context),
+              type: InkType.sparkle,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      clipBehavior: Clip.antiAlias,
+                      child: Hero(
+                        tag: product.imageUrls[0],
+                        child: CachedNetworkImage(
+                          imageUrl: product.imageUrls[0],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.title,
+                          style: AppFonts.headingSmall,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          '${product.price.toString()}\u20BC',
+                          style: AppFonts.bodyLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          product.description,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,
+                          style: AppFonts.bodyMedium.copyWith(color: ColorConstants.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           AppInkWell(
