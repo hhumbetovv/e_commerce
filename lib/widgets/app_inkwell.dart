ddc273728efe9a1997a3c7d64b2ffe2ae12f1a61
hhumbetovv <hhumbetovv@gmail.com>
2023-05-17 13:39:26 +0400

    onLongPress parameter added to AppInkWell
    onTap turned to nullable

--- lib/widgets/app_inkwell.dart
+++ lib/widgets/app_inkwell.dart
@@ -7,34 +7,39 @@ class AppInkWell extends StatelessWidget {
   const AppInkWell({
     Key? key,
     required this.child,
-    required this.onTap,
     required this.type,
+    this.onTap,
+    this.onLongPress,
   }) : super(key: key);
 
   final Widget child;
   final InkType type;
-  final Function() onTap;
+  final Function()? onTap;
+  final Function()? onLongPress;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.antiAlias,
-      alignment: Alignment.center,
-      fit: StackFit.passthrough,
-      children: [
-        child,
-        Positioned.fill(
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              highlightColor: type.value == null ? Colors.transparent : ColorConstants.white.withOpacity(0.4),
-              splashColor: type.value == null ? Colors.transparent : ColorConstants.white.withOpacity(0.4),
-              splashFactory: type.value,
-              onTap: onTap,
-            ),
-          ),
-        )
-      ],
-    );
+    return onTap == null
+        ? child
+        : Stack(
+            clipBehavior: Clip.antiAlias,
+            alignment: Alignment.center,
+            fit: StackFit.passthrough,
+            children: [
+              child,
+              Positioned.fill(
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    highlightColor: type.value == null ? Colors.transparent : ColorConstants.white.withOpacity(0.4),
+                    splashColor: type.value == null ? Colors.transparent : ColorConstants.white.withOpacity(0.4),
+                    splashFactory: type.value,
+                    onTap: onTap,
+                    onLongPress: onLongPress,
+                  ),
+                ),
+              )
+            ],
+          );
   }
 }
